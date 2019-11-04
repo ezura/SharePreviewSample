ed169e835af313b81c312a75c9465eb1671c67d6
ezura <yuka.ezura@gmail.com>
2019-11-04 14:13:40 +0900

    Add share cases

--- ShareSample/ViewController.swift
+++ ShareSample/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         didSet {
             tableView.register(UITableViewCell.self,
                                forCellReuseIdentifier: "default")
+            tableView.contentInset.bottom = 500 // workaround: To avoid that share sheet hides cells. 
             tableView.dataSource = self
             tableView.delegate = self
         }
@@ -25,7 +26,43 @@ class ViewController: UIViewController {
         [.url()],
         [.image()],
         [.url(), .text()],
-        [.text(), .image()]
+        [.text(), .image()],
+        [.image(), .text()],
+        [.itemSource(item: URL(string: "https://www.google.com")!,
+                     linkMetadata: LPLinkMetadata().setTitle(title: "custom title"))],
+        [.itemSource(item: URL(string: "https://www.google.com")!,
+                     linkMetadata: nil)],
+        [.text(), .itemSource(item: URL(string: "https://www.google.com")!,
+        linkMetadata: nil)],
+        [.image(), .itemSource(item: URL(string: "https://www.google.com")!,
+        linkMetadata: nil)],
+        [.url(), .itemSource(item: "text item",
+        linkMetadata: nil)],
+        [.url(), .itemSource(item: "text item",
+        linkMetadata: LPLinkMetadata())],
+        [.url(),
+        .itemSource(item: "text item",
+        linkMetadata: LPLinkMetadata().setTitle(title: "custom title")),
+        // first win?
+        .itemSource(item: #imageLiteral(resourceName: "ezura.png"),
+               linkMetadata: LPLinkMetadata()),],
+        [.url(),
+        // first win?
+        .itemSource(item: #imageLiteral(resourceName: "ezura.png"),
+               linkMetadata: LPLinkMetadata()),
+        .itemSource(item: "text item",
+        linkMetadata: LPLinkMetadata().setTitle(title: "custom title")),],
+        [.url(), 
+         .itemSource(item: URL(string: "https://developer.apple.com")!,
+                     linkMetadata: nil)],
+        [.itemSource(item: URL(string: "https://developer.apple.com")!,
+        linkMetadata: nil),
+         .url(),
+         .text("test")],
+        [.text("test"),
+         .itemSource(item: URL(string: "https://developer.apple.com")!,
+        linkMetadata: nil),
+         .url()],
     ]
 }
 
@@ -69,8 +106,9 @@ extension ViewController: UITableViewDelegate {
 
 enum ShareItem {
     case text(String = "sample text")
-    case url(URL = URL(string: "https://novel.line.me/")!)
+    case url(URL = URL(string: "https://www.google.com")!)
     case image(UIImage = #imageLiteral(resourceName: "ezura.png"))
+    case itemSource(item: Any, linkMetadata: LPLinkMetadata?)
     
     var contents: Any {
         switch self {
@@ -78,6 +116,8 @@ enum ShareItem {
              .url(let v as Any),
              .image(let v as Any):
             return v
+        case .itemSource(let item, let linkMetadata):
+            return ShareActivityItemSource(item: item, linkMetadata: linkMetadata)
         }
     }
     
@@ -94,6 +134,53 @@ enum ShareItem {
             let text = NSMutableAttributedString(string: "Image: ")
             text.append(NSAttributedString(attachment: attachment))
             return text
+        case .itemSource(let item, let linkMetadata):
+            let text = """
+                       ItemSource:
+                           - item: \(String(describing: item))
+                           - metadata: \(linkMetadata?.debugDescription ?? "nil")
+                       """
+            return NSAttributedString(string: text)
         }
+    }
+}
+
+class ShareActivityItemSource: NSObject, UIActivityItemSource {
+    let item: Any
+    let linkMetadata: LPLinkMetadata?
+    
+    init(item: Any, linkMetadata: LPLinkMetadata?) {
+        self.item = item
+        self.linkMetadata = linkMetadata
+    }
+
+    func activityViewControllerLinkMetadata(_ activityViewController: UIActivityViewController) -> LPLinkMetadata? {
+        linkMetadata
+    }
+    
+    func activityViewControllerPlaceholderItem(_ activityViewController: UIActivityViewController) -> Any {
+        item
+    }
+    
+    func activityViewController(_ activityViewController: UIActivityViewController, itemForActivityType activityType: UIActivity.ActivityType?) -> Any? {
+        item
+    }
+    
+}
+
+private extension LPLinkMetadata {
+    func setTitle(title: String) -> LPLinkMetadata {
+        self.title = title
+        return self
+    }
+}
+
+private extension LPLinkMetadata {
+    var visualizeString: String {
+        return """
+        - title: \(title ?? "nil")
+        - image: \(self.imageProvider?.suggestedName ?? "nil")
+        - originalURL: \(self.originalURL?.debugDescription ?? "nil")
+        """
     }
 }
